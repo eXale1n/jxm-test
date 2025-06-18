cc6d5d47f35fe3cbaf22cff7994acf931c28ea42
eXale1n <falkvg@gmail.com>
2025-06-18 21:51:11 +0800

    Fixed repo url to github.io.

--- qualtrics.js
+++ qualtrics.js
@@ -63,7 +63,7 @@ Qualtrics.SurveyEngine.addOnReady(function () {
 	console.log("   selected valence messages:", msgs);
 
 	// construct iframe url with game settings
-	var src = "https://github.com/eXale1n/jxm-test"
+	var src = "https://eXale1n.github.io/jxm-test"
 		+ "?competition=" + encodeURIComponent(competition)
 		+ "&valence="     + encodeURIComponent(valence)
 		+ "&mode="        + encodeURIComponent(mode)
