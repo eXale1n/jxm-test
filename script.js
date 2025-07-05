c6b8989552daf53b7b3a1d991e821e4b5eac2e10
eXale1n <falkvg@gmail.com>
2025-07-05 15:08:08 +0800

    Cross-Origin Hack.

--- script.js
+++ script.js
@@ -714,10 +714,8 @@
         lastTime = performance.now();
         startCountdown();
       } else {
-        // after two rounds, go on to the next section
-        window.parent.document
-              .getElementById('NextButton')
-              .click();
+        // tell Qualtrics “advance me”
+        window.parent.postMessage({ type: 'soloContinue' }, '*');
         // or if you want to just reload
         // location.reload();
       }
